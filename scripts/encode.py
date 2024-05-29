f01a2ff209053addf76d1386521afa5cc4a1bd91
Max Belitsky <md.belitsky@gmail.com>
2024-05-29 21:09:24 +0200

    Fix and refactor encode.py

--- scripts/encode.py
+++ scripts/encode.py
@@ -1,108 +1,59 @@
 import argparse
-import json
 import os
-from collections import defaultdict
 
 import torch
 from torch.utils.data import DataLoader
 from transformers import AutoModelForMaskedLM, AutoTokenizer
+from tqdm.auto import tqdm
 
 from cross_lingual_subnets.constants import Experiments
+from cross_lingual_subnets.utils import mean_pooling
+from cross_lingual_subnets.data import get_bible_dataloaders_by_language
+from scripts.create_subsets import WIKIPEDIA_DUMPS
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
-
-def mean_pooling(hidden_state, attention_mask):
-    input_mask_expanded = (
-        attention_mask.unsqueeze(-1).expand(hidden_state.size()).float()
-    )
-    return torch.sum(hidden_state * input_mask_expanded, 1) / torch.clamp(
-        input_mask_expanded.sum(1), min=1e-9
-    )
+experiment_map = {
+    "ar": Experiments.AR_SUB_MLM_FINETUNED,
+    "de": Experiments.DE_SUB_MLM_FINETUNED,
+    "en": Experiments.EN_SUB_MLM_FINETUNED,
+    "es": Experiments.ES_SUB_MLM_FINETUNED,
+    "hi": Experiments.HI_SUB_MLM_FINETUNED,
+    "ru": Experiments.RU_SUB_MLM_FINETUNED,
+    "zh": Experiments.ZH_SUB_MLM_FINETUNED,
+}
 
 
 def encode(
     dataloader: DataLoader,
     model,
-    tokenizer,
     language: str,
-    experiment_name=Experiments.XLMR_BASE,
+    output_dir: str,
+    experiment_name,
 ):
     # Save hidden outputs per layer
     hids = {}
-    for i, batch in enumerate(dataloader):
-        print(f"Predicting batch {i+1}/{len(dataloader)}")
+    for i, batch in enumerate(tqdm(dataloader)):
 
         # Note! First layer here is embedding layer
         with torch.no_grad():
             output = model(**batch, output_hidden_states=True)["hidden_states"]
 
-        # FIXME: get rid of the loop
         # Aggregate averages of sentences (hence mean) per layer
         for j in range(len(output)):
+            out = mean_pooling(output[j], batch["attention_mask"])
             if i == 0:
-                hids[j] = mean_pooling(output[j], batch["attention_mask"])
+                hids[j] = out
             else:
-                out = mean_pooling(output[j], batch["attention_mask"])
                 hids[j] = torch.cat((hids[j], out), 0)
 
-    exp_destination = f"data/encodings/{experiment_name}/{language}.pt"
-    print(f"Saving encodings to {exp_destination}")
-    if not os.path.exists(f"data/encodings/{experiment_name}/"):
-        os.makedirs(f"data/encodings/{experiment_name}/")
-
-    torch.save(hids, exp_destination)
-
-
-def collate_batch(batch, tokenizer):
-    encoded_input = tokenizer(batch, return_tensors="pt", padding=True)
-    return encoded_input
-
-
-def get_bible_dataloaders_by_language(
-    args, tokenizer, bible_path: str = "data/text/bible_parallel_corpus.json"
-) -> tuple[dict, list]:
-    with open(bible_path, "r", encoding="ISO-8859-1") as f:
-        texts = json.load(f)
-
-    texts_by_language = defaultdict(list)
-    languages = texts[0].keys()
-    for text in texts:
-        for lang in languages:
-            texts_by_language[lang].append(text[lang])
-
-    # Define a custom collate function which takes in a tokenzier
-    collate_fn = lambda batch: collate_batch(batch, tokenizer=tokenizer)  # noqa
-
-    data_loader_kwargs = {
-        "batch_size": args.batch_size,
-        "collate_fn": collate_fn,
-        "pin_memory": True,
-    }
-
-    dataloaders = {
-        language: DataLoader(
-            texts_by_language[language][: args.max_sentences], **data_loader_kwargs
-        )
-        for language in languages
-    }
-
-    return dataloaders, languages
+    # Save encodings
+    os.makedirs(f"{output_dir}/{experiment_name}/", exist_ok=True)
+    save_path = f"{output_dir}/{experiment_name}/{language}.pt"
+    print(f"Saving encodings to {save_path}")
+    torch.save(hids, save_path)
 
 
 if __name__ == "__main__":
-    pairs = [
-        ("FacebookAI/xlm-roberta-base", Experiments.XLMR_BASE),
-        ("artifacts/xlmr-mlm-finetuned", Experiments.XLMR_MLM_FINETUNED),
-        ("artifacts/pruned_ar_mlm_finetuned", Experiments.AR_SUB_MLM_FINETUNED),
-        ("artifacts/pruned_de_mlm_finetuned", Experiments.DE_SUB_MLM_FINETUNED),
-        ("artifacts/pruned_en_mlm_finetuned", Experiments.EN_SUB_MLM_FINETUNED),
-        ("artifacts/pruned_es_mlm_finetuned", Experiments.ES_SUB_MLM_FINETUNED),
-        ("artifacts/pruned_hi_mlm_finetuned", Experiments.HI_SUB_MLM_FINETUNED),
-        ("artifacts/pruned_ru_mlm_finetuned", Experiments.RU_SUB_MLM_FINETUNED),
-        ("artifacts/pruned_zh_mlm_finetuned", Experiments.ZH_SUB_MLM_FINETUNED),
-    ]
-
     parser = argparse.ArgumentParser(
         description="Encoding sentences using language models"
     )
@@ -119,23 +70,86 @@ if __name__ == "__main__":
         help="The amount of sentences to encode",
         default=1000,
     )
+    parser.add_argument(
+        "--data_path",
+        default="data/text/bible_parallel_corpus.json",
+        type=str,
+        help="The path to the data file",
+    )
+    parser.add_argument(
+        "--output_dir",
+        default="data/encodings",
+        type=str,
+        help="Encodings output directory",
+    )
+    parser.add_argument(
+        "--pruned_models_dir_prefix",
+        type=str,
+        default="artifacts/pruned",
+        help="The prefix of the pruned models directory",
+    )
+    parser.add_argument(
+        "--pruned_models_dir_postfix",
+        type=str,
+        default="mlm_finetuned",
+        help="The prefix of the pruned models directory",
+    )
+    parser.add_argument(
+        "--languages",
+        type=str,
+        nargs="*",
+        required=False,
+        choices=WIKIPEDIA_DUMPS.keys(),
+        default=["en", "de", "es", "hi", "ru", "zh", "ar"],
+        help=("The languages of subnetworks to use for encoding"),
+    )
+    parser.add_argument(
+        "--use_base",
+        action=argparse.BooleanOptionalAction,
+        help="Use the base model for encoding",
+    )
+    parser.add_argument(
+        "--use_mlm_finetuned",
+        action=argparse.BooleanOptionalAction,
+        help="Use the MLM finetuned model for encoding",
+    )
     args = parser.parse_args()
+
+    # Load language specific subnetworks
+    pairs = [
+        (
+            f"{args.pruned_models_dir_prefix}_{lang}_{args.pruned_models_dir_postfix}",
+            experiment_map[lang],
+        )
+        for lang in args.languages
+    ]
+
+    # Add base and MLM finetuned models
+    if args.use_base:
+        pairs.append(("FacebookAI/xlm-roberta-base", Experiments.XLMR_BASE))
+    if args.use_mlm_finetuned:
+        pairs.append(("artifacts/xlmr-mlm-finetuned", Experiments.XLMR_MLM_FINETUNED))
 
     print("Loading tokenizer...")
     tokenizer = AutoTokenizer.from_pretrained("FacebookAI/xlm-roberta-base")
 
-    dataloaders, languages = get_bible_dataloaders_by_language(args, tokenizer)
+    dataloaders, languages = get_bible_dataloaders_by_language(
+        max_sentences=args.max_sentences,
+        tokenizer=tokenizer,
+        batch_size=args.batch_size,
+        path=args.data_path,
+    )
 
     for checkpoint, experiment in pairs:
         print("Loading model...")
         model = AutoModelForMaskedLM.from_pretrained(checkpoint)
 
         for language in languages:
-            print(f"=== {language} ===")
+            print(f"Encoding with a === {language} === subnetwork")
             encode(
                 dataloader=dataloaders[language],
                 model=model,
-                tokenizer=tokenizer,
                 language=language,
                 experiment_name=experiment,
+                output_dir=args.output_dir,
             )
